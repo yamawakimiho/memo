56268c2666a176f339eeba66e1292426762f09c0
yamawakimiho <cab235@gmail.com>
2022-06-15 11:07:55 -0300

    Added heroku to allowed hosts in settings.py

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", ".researchthroughdesign.org"]
 os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
 # Application definition
