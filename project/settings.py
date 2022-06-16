8d71377e9fe5971de2c4e563e35b0d732caa2bdf
yamawakimiho <cab235@gmail.com>
2022-06-16 14:56:26 -0300

    Added validation when adding or updating card

--- project/settings.py
+++ project/settings.py
@@ -7,7 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = [".herokuapp.com",'localhost']
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
