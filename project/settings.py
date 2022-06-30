874acad3cfb4461c312443ba05e60c2cdd8aa62e
yamawakimiho <cab235@gmail.com>
2022-06-30 00:17:28 -0300

    Add pagination on DRF + template, favicon

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 9
 }
 
 # Sucessful login
